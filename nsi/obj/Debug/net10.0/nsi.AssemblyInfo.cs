dddaf8a31cb46056b848e7f4dcabd707e8f8a6b6
StrLnx <galkindeman79@gmail.com>
2026-06-26 11:56:29 +0300

    1

--- nsi/obj/Debug/net10.0/nsi.AssemblyInfo.cs
+++ nsi/obj/Debug/net10.0/nsi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nsi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed978e94397229addc30559ba664561af69fc3f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("nsi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nsi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
